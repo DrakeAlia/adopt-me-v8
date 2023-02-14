c7e6f5cf3a27fe8cb1adf765f3c4c7d16aca35c9
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-02-14 15:04:43 -0800

    added handling events in class components in carousel file

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,16 +9,29 @@ class Carousel extends Component {
         images: ["http://pets-images.dev-apis.com/pets/none.jpg"]
     }
 
+    handleIndexClick = (e) => {
+        this.setState({
+            // +: takes a string and turns it into a number
+            active: +e.target.dataset.index
+        })
+    }
+
     render() {
         const { active } = this.state
         const { images } = this.props
+        // Cannot use hooks here because we are using class component here :(
+        // Also Error Boundaries must be written as a class component
 
         return (
             <div className='carousel'>
                 <img src={images[active]} alt="animal hero" />
                 <div className='carousel-smaller'>
                     {images.map((photo, index) => (
+                        // eslint-disable-next-line
                         <img
+                            onClick={this.handleIndexClick}
+                            // * Everything that is in the DOM is a string, even a number
+                            data-index={index}
                             key={photo}
                             src={photo}
                             className={index === active ? "active" : ""}
